e9ffdcebeb2c90a018e8220831222b13cb1ae56b
Doonywoony <javismcarr@gmail.com>
2022-02-12 11:40:24 -0500

    created createdAccount.js/ createAccount.handlebars for user sign up

---
+++ public/js/createAccount.js
@@ -0,0 +1,24 @@
+// signup handler
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ username, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    response.ok
+      ? document.location.replace("/profile")
+      : alert(response.statusText);
+  }
+};
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
