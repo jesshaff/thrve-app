bee9fcc84c092f0cf43c48ebaa42b1961bf27207
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2022-02-15 20:05:28 -0500

    fixed issues with login css,js, and html - still not loading login page WIP

--- public/js/login.js
+++ public/js/login.js
@@ -1,57 +1,57 @@
 const placeholders = document.querySelectorAll(
-    ".styled-input__placeholder-text"
-  ),
+  ".styled-input__placeholder-text"),
+  
   inputs = document.querySelectorAll(".styled-input__input");
-
-placeholders.forEach(function (el, i) {
-  let value = el.innerText,
+  
+  placeholders.forEach(function (el, i) {
+    let value = el.innerText,
     html = "";
-  for (let w of value) {
-    if (!value) value = "&nbsp;";
-    html += `<span class="letter">${w}</span>`;
-  }
-  el.innerHTML = html;
-});
-
-inputs.forEach(function (el) {
-  let parent = el.parentNode;
-  el.addEventListener(
+    for (let w of value) {
+      if (!value) value = "&nbsp;";
+      html += `<span class="letter">${w}</span>`;
+    }
+    el.innerHTML = html;
+  });
+  
+  inputs.forEach(function (el) {
+    let parent = el.parentNode;
+    el.addEventListener(
     "focus",
     function () {
       parent.classList.add("filled");
       placeholderAnimationIn(parent, true);
     },
-    false
-  );
-  el.addEventListener(
+    false);
+  
+    el.addEventListener(
     "blur",
     function () {
       if (el.value.length) return;
       parent.classList.remove("filled");
       placeholderAnimationIn(parent, false);
     },
-    false
-  );
-});
-
-function placeholderAnimationIn(parent, action) {
-  let act = action ? "add" : "remove";
-  let letters = parent.querySelectorAll(".letter");
-  letters = [].slice.call(letters, 0);
-  if (!action) letters = letters.reverse();
-  letters.forEach(function (el, i) {
-    setTimeout(function () {
-      let contains = parent.classList.contains("filled");
-      if ((action && !contains) || (!action && contains)) return;
-      el.classList[act]("active");
-    }, 50 * i);
+    false);
+  
   });
-}
-
-setTimeout(function () {
-  document.body.classList.add("on-start");
-}, 100);
-
-setTimeout(function () {
-  document.body.classList.add("document-loaded");
-}, 1800);
+  
+  function placeholderAnimationIn(parent, action) {
+    let act = action ? "add" : "remove";
+    let letters = parent.querySelectorAll(".letter");
+    letters = [].slice.call(letters, 0);
+    if (!action) letters = letters.reverse();
+    letters.forEach(function (el, i) {
+      setTimeout(function () {
+        let contains = parent.classList.contains("filled");
+        if (action && !contains || !action && contains) return;
+        el.classList[act]("active");
+      }, 50 * i);
+    });
+  }
+  
+  setTimeout(function () {
+    document.body.classList.add("on-start");
+  }, 100);
+  
+  setTimeout(function () {
+    document.body.classList.add("document-loaded");
+  }, 1800);
