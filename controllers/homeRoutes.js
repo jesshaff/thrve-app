44a1eeb7ef45d0c925ccfebdd62171ed1a6f6203
Jesse Lawler <jedd.lawler@gmail.com>
2022-02-17 01:26:03 -0700

    ugh

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -34,7 +34,7 @@ router.get("/profile", withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
-
+// Redirect working, not much else...
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
