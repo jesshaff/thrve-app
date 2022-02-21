776b107955bb1ee3057fd1db2a5895b32f7c6294
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2022-02-20 19:49:50 -0500

    working to fix API data to finish charts

--- public/js/charts-mood.js
+++ public/js/charts-mood.js
@@ -1,8 +1,22 @@
-const DUMMY_DATA = [
-    { id: 'd1', value: 10, region: 'USA'},
-    { id: 'd2', value: 8, region: 'India'},
-    { id: 'd3', value: 16, region: 'China'},
-];
+// const fs = require('../../utils/fsUtils');
+
+const { response } = require("express");
+
+// const DUMMY_DATA = [
+//     { id: 'd1', value: 10, region: 'USA'},
+//     { id: 'd2', value: 8, region: 'India'},
+//     { id: 'd3', value: 16, region: 'China'},
+// ];
+
+// async function userData() {
+//     const response = await fetch('./api/profile', {
+//       method: 'POST',
+//       body: JSON.stringify(data)
+//     });
+//     const responseText = await response.text();
+//     console.log(responseText); // logs 'OK'
+//   }
+//   userData();
 
 // const moodDataExample = fetch('/seeds/moodData.json').then(function (response) {
 //   return response.json();
@@ -12,22 +26,22 @@ const DUMMY_DATA = [
 //   console.error('Something went wrong with retrieving the userData');
 // });
 
-// // Get user rating by date for graph
-// const DUMMY_DATA = await fetch("/api/mood", {
-//     method: "GET",
-//     body: JSON.stringify({ date_added, rating }),
-//     headers: { "Content-Type": "application/json" },
-//   });
+// Get user rating by date for graph
+const moodDataExample = fetch("/api/mood", {
+    method: "GET",
+    body: JSON.stringify({ date_added, rating }),
+    headers: { "Content-Type": "application/json" },
+  });
 
-//   if (response.ok) {
-//     document.location.replace("/");
-//   } else {
-//     alert("Failed");
-//   };
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert("Failed");
+  };
 
 const xScale = d3
     .scaleBand()
-    .domain(DUMMY_DATA.map((dataPoint) => dataPoint.region))
+    .domain(moodDataExample.map((dataPoint) => dataPoint.user_id))
     .rangeRound([0, 250]) // 250 is width of container in css
     .padding(0.1);  
 const yScale = d3.scaleLinear().domain([0, 20]).range([100, 0]); // 20 is the value -- a few higher than the highest value in the data ex: 16 for China
@@ -44,7 +58,7 @@ const container = d3.select('svg').classed('container', true)  // if using class
 
 const bars = container
 .selectAll('.bar')
-.data(DUMMY_DATA)
+.data(moodDataExample)
 .enter()  // data not rendered yet
 .append('rect') // append div for missing element
 .classed('bar', true)
